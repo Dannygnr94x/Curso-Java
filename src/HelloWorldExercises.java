ee357ee61af55cf85079d62faaac8ed0bf815c32
Dannygnr94x <lukas.mune@gmail.com>
2025-11-04 18:37:03 +0100

    subida ejercicios 1

---
+++ src/HelloWorldExercises.java
@@ -0,0 +1,37 @@
+
+void main(String[] args) {
+
+    // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+        System.out.println("Daniel");
+    // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+        System.out.println("Hola, \nmundo");
+    // 3. Añade un comentario sobre lo que hace cada línea del programa.
+        // Linea 5: Imprime mi nombre.
+        //Linea 7: Imprime en dos lineas con un solo print
+    // 4. Crea un comentario en varias líneas.
+        /* Esto es
+        un comentario
+        en varias líneas
+         */
+
+    // 5. Imprime tu edad, tu color favorito y tu ciudad.
+        System.out.println("22, \nNegro, \nMadrid");
+
+    // 6. Explora los diferentes System.XXX.println(); más allá de "out".
+        System.err.println("Error 404" );
+    // 7. Utiliza varios println para imprimir una frase.
+        System.out.print("Hola, ");
+        System.out.print("mi nombre es ");
+        System.out.println("Daniel.");
+    // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
+        System.out.println("       ******");
+        System.out.println("     *        *");
+        System.out.println("    *   o   o  *");
+        System.out.println("    *     D    *");
+        System.out.println("     * (_____)*");
+        System.out.println("      *******");
+    // 9. Intenta ejecutar el programa sin el método main y observa el error.
+
+    //10. Intenta cambiar el nombre del archivo a uno diferente del de la clase y compílalo. ¿Qué pasa?
+
+}
